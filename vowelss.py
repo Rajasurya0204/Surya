7638d3355b1ba3a9293932d830f1388bd06be33c
Rajasurya0204 <47408513+Rajasurya0204@users.noreply.github.com>
2019-02-07 11:58:50 +0530

    Rename vowels.py to vowelss.py

---
+++ vowelss.py
@@ -0,0 +1,5 @@
+n =input("")
+if(n =='A' or n =='a' or n =='E' or n =='e' or n =='I' or n =='i' or n =='O' or n =='o' or n =='U' or n =='u'):
+    print ("Vowel")
+else:
+    print ("is a Consonant")
