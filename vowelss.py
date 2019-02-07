216792297b9a896e53bd6c438a773dc9332d847f
Rajasurya0204 <47408513+Rajasurya0204@users.noreply.github.com>
2019-02-07 12:01:28 +0530

    Update vowelss.py

--- vowelss.py
+++ vowelss.py
@@ -2,4 +2,4 @@ n =input("")
 if(n =='A' or n =='a' or n =='E' or n =='e' or n =='I' or n =='i' or n =='O' or n =='o' or n =='U' or n =='u'):
     print ("Vowel")
 else:
-    print ("is a Consonant")
+    print ("Consonant")
